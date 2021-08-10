5cf1e5c0d462a30b696d298e44706298e2e040e5
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2021-08-10 21:12:54 +0100

    Switching to utyemma branch

---
+++ app/Http/Controllers/Agent/compileAgents.php
@@ -0,0 +1,16 @@
+<?php
+
+trait CompileAgents{
+
+    public function formatAgentData($agents){
+        $array = [];
+        if (count($agents) > 0) {
+            foreach ($agents as $key => $agent) {
+                $array[] = array_merge($agent->toArray(), ['avatar' => json_decode($agent->avatar)[0]]);  
+            }
+        }
+
+        return $array;
+    }
+    
+}
